040fbeec7f1a3061d22c5b5f372cec07c5a3fb56
yongle12 <kwayongle54@gmail.com>
2026-02-21 23:48:49 +0800

    edit chatbot for admin to reply

--- EcoEatsAdmin.aspx.designer.cs
+++ EcoEatsAdmin.aspx.designer.cs
@@ -87,6 +87,15 @@ namespace Business_App_Dev
         protected global::System.Web.UI.WebControls.Label lblFeedbackMsg;
 
         /// <summary>
+        /// ddlChatStatus control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlChatStatus;
+
+        /// <summary>
         /// rptChats control.
         /// </summary>
         /// <remarks>
